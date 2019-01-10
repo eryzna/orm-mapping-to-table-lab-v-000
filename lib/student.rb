9a7940b434e434d9aa8d10bd45d990abc168be45
Elana Ryznar <eryzna@gmail.com>
2019-01-10 23:00:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,7 @@ class Student
  attr_accessor :grade, :name
  attr_reader :id
 
- def initialize (grade:, name:, id:nil)
-   @grade=grade
-   @name=name
-   @id=id
+ def initialize 
  end  # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
