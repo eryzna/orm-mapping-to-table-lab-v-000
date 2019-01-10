5adbef4d3be32b9a650da074a5cf5385f0b71af0
Elana Ryznar <eryzna@gmail.com>
2019-01-10 23:11:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
  attr_accessor :grade, :name, :id
- #attr_reader :id
+ attr_reader :id
 
- def initialize (name, grade id=nil)
+ def initialize (name, grade, id=nil)
    @grade=grade
    @name=name
    @id=id
